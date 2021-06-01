093e6a94cf8c6cb10c856077af8946d5a0ba4174
GabrielMart-hub <gabriel.pereira.1326853@sga.pucminas.br>
2021-06-01 19:01:47 -0300

    UPDATE nome usuario

--- Codigo/templategabriel/js/index.js
+++ Codigo/templategabriel/js/index.js
@@ -2,11 +2,15 @@ onload = () => {
     const btnPerfil = document.querySelector('.confirma-login');
     const dropMenu = document.querySelector('.dropdown-menu-navbar');
     const alertLogin = document.querySelector('.alert-login');
+    const usuarioSession = JSON.parse(sessionStorage.getItem('usuario-login'));
     btnPerfil.onclick = () => {
-        const usuarioSession = sessionStorage.getItem('usuario-login');
         if(!usuarioSession) {
             dropMenu.classList.add('d-none');
             alertLogin.classList.remove('d-none');
         }
     }
+    btnPerfil.innerHTML = `
+            <i class="ni ni-circle-08"></i>
+            <span class="nav-link-inner--text">${usuarioSession.nome}</span>    
+        `;
 }
