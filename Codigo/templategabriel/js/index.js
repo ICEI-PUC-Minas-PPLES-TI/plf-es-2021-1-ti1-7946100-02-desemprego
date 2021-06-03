0a36b8bcea95d4a43d790b0f3bc0e8226f45a0bb
GabrielMart-hub <gabriel.pereira.1326853@sga.pucminas.br>
2021-06-03 14:14:37 -0300

    UPDATE localhost para session

--- Codigo/templategabriel/js/index.js
+++ Codigo/templategabriel/js/index.js
@@ -3,7 +3,7 @@ onload = () => {
     const alertLogin = document.querySelector('.alert-login');
     const dropdowMenuNavbar = document.querySelector('.dropdown-menu-navbar');
     btnPerfil.onclick = () => {
-        const usuarioSession = localStorage.getItem('usuario-login');
+        const usuarioSession = sessionStorage.getItem('usuario-login');
         if(!usuarioSession) {
             dropdowMenuNavbar.classList.add('d-none');
             alertLogin.classList.remove('d-none');
