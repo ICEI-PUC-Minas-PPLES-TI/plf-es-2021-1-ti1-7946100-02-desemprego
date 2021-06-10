1b9ccdde929f42a9e7d94a715556e03f17897b43
GabrielMart-hub <gabriel.pereira.1326853@sga.pucminas.br>
2021-06-10 19:03:41 -0300

    UPDATE index.html exibir info vagas

--- Codigo/templategabriel/js/index.js
+++ Codigo/templategabriel/js/index.js
@@ -1,8 +1,14 @@
 onload = () => {
+    const usuarioSession = JSON.parse(sessionStorage.getItem('usuario-login'));
+    
+    menuUsuario(usuarioSession);
+    exibirCampoVagas();
+}
+
+function menuUsuario(usuarioSession) {
     const btnPerfil = document.querySelector('.confirma-login');
     const alertLogin = document.querySelector('.alert-login');
     const dropdowMenuNavbar = document.querySelector('.dropdown-menu-navbar');
-    const usuarioSession = JSON.parse(sessionStorage.getItem('usuario-login'));
     if(usuarioSession) {
         btnPerfil.innerHTML = `
         <i class="ni ni-circle-08"></i>
@@ -14,4 +20,31 @@ onload = () => {
             alertLogin.classList.remove('d-none');
         }
     }
+}
+
+function exibirCampoVagas() {
+    const campoVagas = document.querySelector('.campo-vagas');
+    const arrayVagas = JSON.parse(localStorage.getItem('cadastroVagas'));
+    const div = criarDiv();
+    campoVagas.appendChild(div);
+
+    for(let vaga of arrayVagas) {
+        const a = criarA();
+        a.setAttribute('class', 'card p-4 mb-3');
+        a.innerHTML = `
+        <h3 class="text-primary">${vaga.funcao}</h3>
+        <p class="text-default">Nome empresa -> Endereço</p>
+        `;
+        div.appendChild(a);
+    }
+}
+
+function criarDiv() {
+    const div = document.createElement('div');
+    return div;
+}
+
+function criarA() {
+    const a = document.createElement('a');
+    return a;
 }
