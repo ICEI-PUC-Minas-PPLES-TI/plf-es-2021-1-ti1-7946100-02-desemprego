cd1e3f44a191688038b8a2f66787c04209954991
GabrielMart-hub <gabriel.pereira.1326853@sga.pucminas.br>
2021-06-13 21:24:16 -0300

    .

--- Codigo/templategabriel/js/login.js
+++ Codigo/templategabriel/js/login.js
@@ -1,41 +1,66 @@
+const form = document.querySelector('#login');
+const userForm = document.querySelector('#usuario');
+const senhaForm = document.querySelector('#senha');
+const alertCampoVazio = document.querySelector('.alert-campo-vazio');
+const alertUsuarioInexistente = document.querySelector('.alert-usuario-inexistente');
+
+sessionStorage.clear();
+
 onload = () => {
-    const form = document.querySelector('#login');
     form.onsubmit = (e) => {
-        const userForm = document.querySelector('#usuario');
-        const senhaForm = document.querySelector('#senha');
-        const alertCampoVazio = document.querySelector('.alert-campo-vazio');
-        const alertUsuarioInexistente = document.querySelector('.alert-usuario-inexistente');
         e.preventDefault();
-        if(userForm.value.length === 0 || senhaForm.value.length === 0) {
-            alertCampoVazio.setAttribute('style', 'display: block !important');
-            if(userForm.value.length === 0) {
-                userForm.classList.add('is-invalid');
-                userForm.parentElement.parentElement.classList.add('has-danger');
-            }
-            if(senhaForm.value.length === 0) {
-                senhaForm.classList.add('is-invalid');
-                senhaForm.parentElement.parentElement.classList.add('has-danger');
-            }
-            return;
-        }
-        const userLocalStorage = JSON.parse(localStorage.getItem('usuario-login'));
-        const userSession = JSON.parse(sessionStorage.getItem('usuario-login'));
-        if(!userSession) {
-            const userObject = {
-                nome: userLocalStorage.nome,
-                senha: userLocalStorage.senha
-            }
-            const userJSON = JSON.stringify(userObject);
-            sessionStorage.setItem('usuario-login', userJSON);
-        }
-        if((userForm.value !== userLocalStorage.nome || userForm.value !== userLocalStorage.email) && senhaForm.value !== userLocalStorage.senha) {
-            alertUsuarioInexistente.setAttribute('style', 'display: block !important');
-            userForm.classList.add('is-invalid');
-            userForm.parentElement.parentElement.classList.add('has-danger');
-            senhaForm.classList.add('is-invalid');
-            senhaForm.parentElement.parentElement.classList.add('has-danger');
-            return;
-        }
+
+        if(!verificarCamposLogin(userForm, senhaForm, alertCampoVazio)) return;
+
+        const userLocalStorage = JSON.parse(localStorage.getItem('usuario-cadastro'));
+        console.log('ola', userLocalStorage);
+        criarSession(userLocalStorage);
+
+        if(!logar(userForm, senhaForm, alertUsuarioInexistente, userLocalStorage)) return;
+        
         location.href = "./index.html";
     }
+}
+
+function logar(usuario, senha, alertUsuarioInexistente, userLocalStorage) {
+    for(let usuarioCadastrado of userLocalStorage) {
+        if((usuario.value !== usuarioCadastrado.nome || usuario.value !== usuarioCadastrado.email) && senha.value !== usuarioCadastrado.senha) {
+            alertUsuarioInexistente.setAttribute('style', 'display: block !important');
+            usuario.classList.add('is-invalid');
+            usuario.parentElement.parentElement.classList.add('has-danger');
+            usuario.classList.add('is-invalid');
+            usuario.parentElement.parentElement.classList.add('has-danger');
+            return false;
+        }
+        if((usuario.value === usuarioCadastrado.nome || usuario.value === usuarioCadastrado.email) && senha.value === usuarioCadastrado.senha) {
+            criarSession(usuario.value, senha.value, usuarioCadastrado.id);
+        }
+    }
+    return true;
+}
+
+function criarSession(nome, senha, id) {
+    const userObject = {
+        id: id,
+        nome: nome,
+        senha: senha
+    }
+    const userJSON = JSON.stringify(userObject);
+    sessionStorage.setItem('usuario-login', userJSON);
+}
+
+function verificarCamposLogin(usuario, senha, alertCampoVazio) {
+    if(usuario.value.length === 0 || senha.value.length === 0) {
+        alertCampoVazio.setAttribute('style', 'display: block !important');
+        if(usuario.value.length === 0) {
+            usuario.classList.add('is-invalid');
+            usuario.parentElement.parentElement.classList.add('has-danger');
+        }
+        if(senha.value.length === 0) {
+            senha.classList.add('is-invalid');
+            senha.parentElement.parentElement.classList.add('has-danger');
+        }
+        return false;
+    }
+    return true;
 }
