dd8d176a2ba78e2c8d19026ea0b472ad8c0f9795
GabrielMart-hub <gabriel.pereira.1326853@sga.pucminas.br>
2021-07-01 21:46:10 -0300

    .

--- Codigo/templategabriel/js/index.js
+++ Codigo/templategabriel/js/index.js
@@ -6,120 +6,117 @@ if(listaFavoritos) {
 
 
 function leDados(){
-    let strDados = localStorage.getItem('cadastroVagas');
+    //let strDados = localStorage.getItem('cadastroVagas');
     let objDados = {};
     let arrayDadosTeste = [];
-    if (strDados){
-        objDados = JSON.parse(strDados);
-    }
-    else{
-        objDados = [
-            {
-                id: 0,
-                atividades: "Buscamos um profissional para atuar como Analista de Dados, realizando integrações de informações em sistemas e muito mais",
-                empresa: "PUC Minas",
-                escolaridade: "Superior completo",
-                funcao: "Analista de dados",
-                requisitos: "Experiência com ferramentas de BI",
-                salario: "2500,00",
-                status: "Aberta",
-                localidade: "Belo Horizonte",
-                filtro: "Mais experiente",
-                data: "19/12/2021"
-            },
-            {
-                id: 1,
-                atividades: "Buscamos um profissional para atuar como administrador de Banco de Dados, realizando integrações de informações em sistemas e muito mais, alem de criar dashbords para tomada de decisão",
-                empresa: "PUC Minas",
-                escolaridade: "Superior completo",
-                funcao: "Administrador de banco de dados",
-                requisitos: "Experiência com ferramentas de BI, SQL, Oracle (será um diferencial)",
-                salario: "4000,00",
-                status: "Aberta",
-                localidade: "Brasil",
-                filtro: "Mais experiente",
-                data: "25/12/2021"
-            },
-            {
-                id: 2,
-                atividades: "Buscamos um estagiario para atuar com UX",
-                empresa: "PUC Minas",
-                escolaridade: "Ensino médio",
-                funcao: "Estágio de UX",
-                requisitos: "Experiência com UX",
-                salario: "1000,00",
-                status: "Aberta",
-                localidade: "Belo Horizonte",
-                filtro: "Primeira Oportunidade",
-                data: "19/12/2021"
-            },
-            {
-                id: 3,
-                atividades: "Atendente de telemarketing, atendimento direto com cliente, resolução de problemas e resiliência",
-                empresa: "PUC Minas",
-                escolaridade: "Ensino médio",
-                funcao: "Atendente de Telemarketing",
-                requisitos: "Educação e análise de perfil, além de muita energia e força de vontade",
-                salario: "2000,00",
-                status: "Aberta",
-                localidade: "São Paulo/Remoto",
-                filtro: "Primeira Oportunidade",
-                data: "19/12/2021"
-            },
-            {
-                id: 4,
-                atividades: "Atendimento ao cliente, apresentação de produtos, elaboração de propostas comercias, envio para correspondente bancário, acompanhamento até aprovação de crédito, fechamento da vendas.",
-                empresa: "PUC Minas",
-                escolaridade: "Ensino Médio",
-                funcao: "Consultor de Vendas",
-                requisitos: "Experiência com vendas e com clientes",
-                salario: "4000,00",
-                status: "Aberta",
-                localidade: "Belo Horizonte",
-                filtro: "Outras Culturas",
-                data: "19/10/2021"
-            },
-            {
-                id: 5,
-                atividades: "Vendas por telefone através dos nossos canais exclusivos ativos e receptivos. Prospecções, reativações e abertura de novos clientes. Manutenção da carteira de clientes e atendimento pós-vendas.",
-                empresa: "PUC Minas",
-                escolaridade: "Ensino Médio",
-                funcao: "Vendedor",
-                requisitos: "Experiência com vendas e com clientes",
-                salario: "3000,00",
-                status: "Aberta",
-                localidade: "Belo Horizonte",
-                filtro: "Outras Culturas",
-                data: "19/10/2021"
-            },
-            {
-                id: 6,
-                atividades: "Experiência em plataformas digitais  na geração de leads para geração de vendas. Acompanhamento do fluxo de vendas de fim a fim. Conhecimento no Power BI/ Dynamics / CRM",
-                empresa: "PUC Minas",
-                escolaridade: "Superior",
-                funcao: "Analista de Inside Sales",
-                requisitos: "Conhecimento de kpis de inside sales. Senso analítico e propositivo mediante as análises destes. Kpis Excel Avançado e pacote office.",
-                salario: "5000,00",
-                status: "Aberta",
-                localidade: "Betim",
-                filtro: "Geral",
-                data: "19/08/2021"
-            },
-            {
-                id: 7,
-                atividades: "Auxiliar na gestão dos projetos corporativos. Identificar e definir escopo dos projetos. Envolver partes interessadas e alinhar expectativas. Construir e acompanhar cronogramas. Identificar e acompanhar riscos e realizar reuniões de status.",
-                empresa: "PUC Minas",
-                escolaridade: "Superior completo",
-                funcao: "Analista de Projetos",
-                requisitos: "Gestão de projetos, Microsoft Project, Pacote Office 365. ",
-                salario: "3000,00",
-                status: "Aberta",
-                localidade: "Belo Horizonte",
-                filtro: "Geral",
-                data: "19/07/2021"
-            }
-        ];
-    }
+    objDados = [
+        {
+            id: 0,
+            atividades: "Buscamos um profissional para atuar como Analista de Dados, realizando integrações de informações em sistemas e muito mais",
+            empresa: "PUC Minas",
+            escolaridade: "Superior completo",
+            funcao: "Analista de dados",
+            requisitos: "Experiência com ferramentas de BI",
+            salario: "2500,00",
+            status: "Aberta",
+            localidade: "Belo Horizonte",
+            filtro: "Mais experiente",
+            data: "19/12/2021"
+        },
+        {
+            id: 1,
+            atividades: "Buscamos um profissional para atuar como administrador de Banco de Dados, realizando integrações de informações em sistemas e muito mais, alem de criar dashbords para tomada de decisão",
+            empresa: "PUC Minas",
+            escolaridade: "Superior completo",
+            funcao: "Administrador de banco de dados",
+            requisitos: "Experiência com ferramentas de BI, SQL, Oracle (será um diferencial)",
+            salario: "4000,00",
+            status: "Aberta",
+            localidade: "Brasil",
+            filtro: "Mais experiente",
+            data: "25/12/2021"
+        },
+        {
+            id: 2,
+            atividades: "Buscamos um estagiario para atuar com UX",
+            empresa: "PUC Minas",
+            escolaridade: "Ensino médio",
+            funcao: "Estágio de UX",
+            requisitos: "Experiência com UX",
+            salario: "1000,00",
+            status: "Aberta",
+            localidade: "Belo Horizonte",
+            filtro: "Primeira Oportunidade",
+            data: "19/12/2021"
+        },
+        {
+            id: 3,
+            atividades: "Atendente de telemarketing, atendimento direto com cliente, resolução de problemas e resiliência",
+            empresa: "PUC Minas",
+            escolaridade: "Ensino médio",
+            funcao: "Atendente de Telemarketing",
+            requisitos: "Educação e análise de perfil, além de muita energia e força de vontade",
+            salario: "2000,00",
+            status: "Aberta",
+            localidade: "São Paulo/Remoto",
+            filtro: "Primeira Oportunidade",
+            data: "19/12/2021"
+        },
+        {
+            id: 4,
+            atividades: "Atendimento ao cliente, apresentação de produtos, elaboração de propostas comercias, envio para correspondente bancário, acompanhamento até aprovação de crédito, fechamento da vendas.",
+            empresa: "PUC Minas",
+            escolaridade: "Ensino Médio",
+            funcao: "Consultor de Vendas",
+            requisitos: "Experiência com vendas e com clientes",
+            salario: "4000,00",
+            status: "Aberta",
+            localidade: "Belo Horizonte",
+            filtro: "Outras Culturas",
+            data: "19/10/2021"
+        },
+        {
+            id: 5,
+            atividades: "Vendas por telefone através dos nossos canais exclusivos ativos e receptivos. Prospecções, reativações e abertura de novos clientes. Manutenção da carteira de clientes e atendimento pós-vendas.",
+            empresa: "PUC Minas",
+            escolaridade: "Ensino Médio",
+            funcao: "Vendedor",
+            requisitos: "Experiência com vendas e com clientes",
+            salario: "3000,00",
+            status: "Aberta",
+            localidade: "Belo Horizonte",
+            filtro: "Outras Culturas",
+            data: "19/10/2021"
+        },
+        {
+            id: 6,
+            atividades: "Experiência em plataformas digitais  na geração de leads para geração de vendas. Acompanhamento do fluxo de vendas de fim a fim. Conhecimento no Power BI/ Dynamics / CRM",
+            empresa: "PUC Minas",
+            escolaridade: "Superior",
+            funcao: "Analista de Inside Sales",
+            requisitos: "Conhecimento de kpis de inside sales. Senso analítico e propositivo mediante as análises destes. Kpis Excel Avançado e pacote office.",
+            salario: "5000,00",
+            status: "Aberta",
+            localidade: "Betim",
+            filtro: "Geral",
+            data: "19/08/2021"
+        },
+        {
+            id: 7,
+            atividades: "Auxiliar na gestão dos projetos corporativos. Identificar e definir escopo dos projetos. Envolver partes interessadas e alinhar expectativas. Construir e acompanhar cronogramas. Identificar e acompanhar riscos e realizar reuniões de status.",
+            empresa: "PUC Minas",
+            escolaridade: "Superior completo",
+            funcao: "Analista de Projetos",
+            requisitos: "Gestão de projetos, Microsoft Project, Pacote Office 365. ",
+            salario: "3000,00",
+            status: "Aberta",
+            localidade: "Belo Horizonte",
+            filtro: "Geral",
+            data: "19/07/2021"
+        }
+    ];
+    localStorage.setItem('cadastroVagas', JSON.stringify(objDados));
+    console.log(objDados);
     return objDados;
 }
 function salvaDados(dados){
@@ -167,6 +164,7 @@ function imprimeDados(){
         }
     }
     card_info.innerHTML = strTexto;
+    selecionarVaga();
     adicionarVagaFavorita();
 }
 function imprimeVagas(){
--- Codigo/templategabriel/js/index_vaga_info.js
+++ Codigo/templategabriel/js/index_vaga_info.js
@@ -1,8 +1,22 @@
 function exibirVaga() {
     const vagaId = Number(JSON.parse(sessionStorage.getItem("vaga-info")).slice(-2));
-    const vagaInfo = JSON.parse();
-    if() {
-
+    const vagasInfos = JSON.parse(localStorage.getItem("cadastroVagas"));
+    const conteudoPagina = document.querySelector(".conteudo-pagina");
+    let str = '';
+    for(let vagaInfo of vagasInfos) {
+        if(vagaInfo.id === vagaId) {
+            str += 
+            `
+                <div class="p-3 rounded">
+                    <h3 class="text-primary">${vagaInfo.funcao}</h3>
+                    <h5 class="text-secondary">${vagaInfo.atividades}</h5>
+                    <div class="">
+                    
+                    </div>
+                </div>
+            `;
+        }
     }
+    conteudoPagina.innerHTML = str;
 }
 exibirVaga();
