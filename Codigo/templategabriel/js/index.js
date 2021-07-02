5a0a33605828851c97de25c4c0d333c03fdf5589
GabrielMart-hub <gabriel.pereira.1326853@sga.pucminas.br>
2021-07-01 22:26:43 -0300

    .

--- Codigo/templategabriel/js/index.js
+++ Codigo/templategabriel/js/index.js
@@ -1,139 +1,143 @@
 const listaFavoritos = JSON.parse(localStorage.getItem('favoritos'));
 let favoritosArray = [];
-if(listaFavoritos) {
+if (listaFavoritos) {
     favoritosArray = [...listaFavoritos];
 }
 
 
-function leDados(){
-    //let strDados = localStorage.getItem('cadastroVagas');
-    let objDados = {};
-    let arrayDadosTeste = [];
-    objDados = [
-        {
-            id: 0,
-            atividades: "Buscamos um profissional para atuar como Analista de Dados, realizando integrações de informações em sistemas e muito mais",
-            empresa: "PUC Minas",
-            escolaridade: "Superior completo",
-            funcao: "Analista de dados",
-            requisitos: "Experiência com ferramentas de BI",
-            salario: "2500,00",
-            status: "Aberta",
-            localidade: "Belo Horizonte",
-            filtro: "Mais experiente",
-            data: "19/12/2021"
-        },
-        {
-            id: 1,
-            atividades: "Buscamos um profissional para atuar como administrador de Banco de Dados, realizando integrações de informações em sistemas e muito mais, alem de criar dashbords para tomada de decisão",
-            empresa: "PUC Minas",
-            escolaridade: "Superior completo",
-            funcao: "Administrador de banco de dados",
-            requisitos: "Experiência com ferramentas de BI, SQL, Oracle (será um diferencial)",
-            salario: "4000,00",
-            status: "Aberta",
-            localidade: "Brasil",
-            filtro: "Mais experiente",
-            data: "25/12/2021"
-        },
-        {
-            id: 2,
-            atividades: "Buscamos um estagiario para atuar com UX",
-            empresa: "PUC Minas",
-            escolaridade: "Ensino médio",
-            funcao: "Estágio de UX",
-            requisitos: "Experiência com UX",
-            salario: "1000,00",
-            status: "Aberta",
-            localidade: "Belo Horizonte",
-            filtro: "Primeira Oportunidade",
-            data: "19/12/2021"
-        },
-        {
-            id: 3,
-            atividades: "Atendente de telemarketing, atendimento direto com cliente, resolução de problemas e resiliência",
-            empresa: "PUC Minas",
-            escolaridade: "Ensino médio",
-            funcao: "Atendente de Telemarketing",
-            requisitos: "Educação e análise de perfil, além de muita energia e força de vontade",
-            salario: "2000,00",
-            status: "Aberta",
-            localidade: "São Paulo/Remoto",
-            filtro: "Primeira Oportunidade",
-            data: "19/12/2021"
-        },
-        {
-            id: 4,
-            atividades: "Atendimento ao cliente, apresentação de produtos, elaboração de propostas comercias, envio para correspondente bancário, acompanhamento até aprovação de crédito, fechamento da vendas.",
-            empresa: "PUC Minas",
-            escolaridade: "Ensino Médio",
-            funcao: "Consultor de Vendas",
-            requisitos: "Experiência com vendas e com clientes",
-            salario: "4000,00",
-            status: "Aberta",
-            localidade: "Belo Horizonte",
-            filtro: "Outras Culturas",
-            data: "19/10/2021"
-        },
-        {
-            id: 5,
-            atividades: "Vendas por telefone através dos nossos canais exclusivos ativos e receptivos. Prospecções, reativações e abertura de novos clientes. Manutenção da carteira de clientes e atendimento pós-vendas.",
-            empresa: "PUC Minas",
-            escolaridade: "Ensino Médio",
-            funcao: "Vendedor",
-            requisitos: "Experiência com vendas e com clientes",
-            salario: "3000,00",
-            status: "Aberta",
-            localidade: "Belo Horizonte",
-            filtro: "Outras Culturas",
-            data: "19/10/2021"
-        },
-        {
-            id: 6,
-            atividades: "Experiência em plataformas digitais  na geração de leads para geração de vendas. Acompanhamento do fluxo de vendas de fim a fim. Conhecimento no Power BI/ Dynamics / CRM",
-            empresa: "PUC Minas",
-            escolaridade: "Superior",
-            funcao: "Analista de Inside Sales",
-            requisitos: "Conhecimento de kpis de inside sales. Senso analítico e propositivo mediante as análises destes. Kpis Excel Avançado e pacote office.",
-            salario: "5000,00",
-            status: "Aberta",
-            localidade: "Betim",
-            filtro: "Geral",
-            data: "19/08/2021"
-        },
-        {
-            id: 7,
-            atividades: "Auxiliar na gestão dos projetos corporativos. Identificar e definir escopo dos projetos. Envolver partes interessadas e alinhar expectativas. Construir e acompanhar cronogramas. Identificar e acompanhar riscos e realizar reuniões de status.",
-            empresa: "PUC Minas",
-            escolaridade: "Superior completo",
-            funcao: "Analista de Projetos",
-            requisitos: "Gestão de projetos, Microsoft Project, Pacote Office 365. ",
-            salario: "3000,00",
-            status: "Aberta",
-            localidade: "Belo Horizonte",
-            filtro: "Geral",
-            data: "19/07/2021"
-        }
-    ];
-    localStorage.setItem('cadastroVagas', JSON.stringify(objDados));
-    console.log(objDados);
+function leDados() {
+    let strDados = localStorage.getItem('cadastroVagas');
+    let objDados;
+    if (strDados) {
+        objDados = JSON.parse(strDados);
+    } else {
+        objDados = [
+            {
+                id: 0,
+                atividades: "Buscamos um profissional para atuar como Analista de Dados, realizando integrações de informações em sistemas e muito mais",
+                empresa: "PUC Minas",
+                escolaridade: "Superior completo",
+                funcao: "Analista de dados",
+                requisitos: "Experiência com ferramentas de BI",
+                salario: "2500,00",
+                status: "Aberta",
+                localidade: "Belo Horizonte",
+                filtro: "Mais experiente",
+                data: "19/12/2021"
+            },
+            {
+                id: 1,
+                atividades: "Buscamos um profissional para atuar como administrador de Banco de Dados, realizando integrações de informações em sistemas e muito mais, alem de criar dashbords para tomada de decisão",
+                empresa: "PUC Minas",
+                escolaridade: "Superior completo",
+                funcao: "Administrador de banco de dados",
+                requisitos: "Experiência com ferramentas de BI, SQL, Oracle (será um diferencial)",
+                salario: "4000,00",
+                status: "Aberta",
+                localidade: "Brasil",
+                filtro: "Mais experiente",
+                data: "25/12/2021"
+            },
+            {
+                id: 2,
+                atividades: "Buscamos um estagiario para atuar com UX",
+                empresa: "PUC Minas",
+                escolaridade: "Ensino médio",
+                funcao: "Estágio de UX",
+                requisitos: "Experiência com UX",
+                salario: "1000,00",
+                status: "Aberta",
+                localidade: "Belo Horizonte",
+                filtro: "Primeira Oportunidade",
+                data: "19/12/2021"
+            },
+            {
+                id: 3,
+                atividades: "Atendente de telemarketing, atendimento direto com cliente, resolução de problemas e resiliência",
+                empresa: "PUC Minas",
+                escolaridade: "Ensino médio",
+                funcao: "Atendente de Telemarketing",
+                requisitos: "Educação e análise de perfil, além de muita energia e força de vontade",
+                salario: "2000,00",
+                status: "Aberta",
+                localidade: "São Paulo/Remoto",
+                filtro: "Primeira Oportunidade",
+                data: "19/12/2021"
+            },
+            {
+                id: 4,
+                atividades: "Atendimento ao cliente, apresentação de produtos, elaboração de propostas comercias, envio para correspondente bancário, acompanhamento até aprovação de crédito, fechamento da vendas.",
+                empresa: "PUC Minas",
+                escolaridade: "Ensino Médio",
+                funcao: "Consultor de Vendas",
+                requisitos: "Experiência com vendas e com clientes",
+                salario: "4000,00",
+                status: "Aberta",
+                localidade: "Belo Horizonte",
+                filtro: "Outras Culturas",
+                data: "19/10/2021"
+            },
+            {
+                id: 5,
+                atividades: "Vendas por telefone através dos nossos canais exclusivos ativos e receptivos. Prospecções, reativações e abertura de novos clientes. Manutenção da carteira de clientes e atendimento pós-vendas.",
+                empresa: "PUC Minas",
+                escolaridade: "Ensino Médio",
+                funcao: "Vendedor",
+                requisitos: "Experiência com vendas e com clientes",
+                salario: "3000,00",
+                status: "Aberta",
+                localidade: "Belo Horizonte",
+                filtro: "Outras Culturas",
+                data: "19/10/2021"
+            },
+            {
+                id: 6,
+                atividades: "Experiência em plataformas digitais  na geração de leads para geração de vendas. Acompanhamento do fluxo de vendas de fim a fim. Conhecimento no Power BI/ Dynamics / CRM",
+                empresa: "PUC Minas",
+                escolaridade: "Superior",
+                funcao: "Analista de Inside Sales",
+                requisitos: "Conhecimento de kpis de inside sales. Senso analítico e propositivo mediante as análises destes. Kpis Excel Avançado e pacote office.",
+                salario: "5000,00",
+                status: "Aberta",
+                localidade: "Betim",
+                filtro: "Geral",
+                data: "19/08/2021"
+            },
+            {
+                id: 7,
+                atividades: "Auxiliar na gestão dos projetos corporativos. Identificar e definir escopo dos projetos. Envolver partes interessadas e alinhar expectativas. Construir e acompanhar cronogramas. Identificar e acompanhar riscos e realizar reuniões de status.",
+                empresa: "PUC Minas",
+                escolaridade: "Superior completo",
+                funcao: "Analista de Projetos",
+                requisitos: "Gestão de projetos, Microsoft Project, Pacote Office 365. ",
+                salario: "3000,00",
+                status: "Aberta",
+                localidade: "Belo Horizonte",
+                filtro: "Geral",
+                data: "19/07/2021"
+            }
+        ];
+        localStorage.setItem('cadastroVagas', JSON.stringify(objDados));
+    }
     return objDados;
 }
-function salvaDados(dados){
+
+function salvaDados(dados) {
     localStorage.setItem('cadastroVagas', JSON.stringify(dados));
 }
-function imprimeDados(){
+
+function imprimeDados() {
     let objDados = leDados();
     let card_info = document.getElementById('card_info');
     let strTexto = '';
     let filtro_info = document.getElementById('filtro_info').value;
-    
-    
-    for (i = 0; i < objDados.length; i++){
+
+
+    for (i = 0; i < objDados.length; i++) {
         const vaga = objDados[i];
-        if (vaga.filtro == filtro_info){
-            strTexto += 
-            `<div class="row">
+        if (vaga.filtro == filtro_info) {
+            strTexto +=
+                `<div class="row">
                 <div class="card container vagas_info">
                     <div class="mb-4 d-flex justify-content-between">
                         <h5><a class="titulo_vaga" id="id-vaga-0${vaga.id}">${vaga.funcao}</a></h5>
@@ -160,24 +164,24 @@ function imprimeDados(){
                         </div>
                     </div>
                 </div>
-            </div>`;            
+            </div>`;
         }
     }
     card_info.innerHTML = strTexto;
     selecionarVaga();
     adicionarVagaFavorita();
 }
-function imprimeVagas(){
+function imprimeVagas() {
     let objDados = leDados();
     let card_info = document.getElementById('card_info');
     let strTexto = '';
     let filtro_info = "Geral"
-    
-    for (i = 0; i < objDados.length; i++){
+
+    for (i = 0; i < objDados.length; i++) {
         const vaga = objDados[i];
-        if (vaga.filtro == filtro_info){
-            strTexto += 
-            `<div class="row">
+        if (vaga.filtro == filtro_info) {
+            strTexto +=
+                `<div class="row">
                 <div class="card container vagas_info">
                     <div class="mb-4 d-flex justify-content-between">
                         <h5><a class="titulo_vaga" id="id-vaga-0${vaga.id}">${vaga.funcao}</a></h5>
@@ -204,7 +208,7 @@ function imprimeVagas(){
                         </div>
                     </div>
                 </div>
-            </div>`;            
+            </div>`;
         }
     }
     card_info.innerHTML = strTexto;
@@ -226,15 +230,15 @@ onload = () => {
     menuUsuario(usuarioSession);
     selecionarVaga();
     adicionarVagaFavorita();
-    
+
 }
 
 
 function selecionarVaga() {
     const linkVagas = document.querySelectorAll('.titulo_vaga');
     console.log(linkVagas);
-    for(let linkVaga of linkVagas) {
-        linkVaga.addEventListener("click", function(e) {
+    for (let linkVaga of linkVagas) {
+        linkVaga.addEventListener("click", function (e) {
             sessionStorage.setItem("vaga-info", JSON.stringify(linkVaga.id));
             location.href = "index_vaga_info.html";
         });
@@ -245,13 +249,13 @@ function menuUsuario(usuarioSession) {
     const btnPerfil = document.querySelector('.confirma-login');
     const alertLogin = document.querySelector('.alert-login');
     const dropdowMenuNavbar = document.querySelector('.dropdown-menu-navbar');
-    if(usuarioSession) {
+    if (usuarioSession) {
         btnPerfil.innerHTML = `
         <i class="ni ni-circle-08"></i>
         <span class="nav-link-inner--text">${usuarioSession.nome}</span>`;
     }
     btnPerfil.onclick = () => {
-        if(!usuarioSession) {
+        if (!usuarioSession) {
             dropdowMenuNavbar.classList.add('d-none');
             alertLogin.classList.remove('d-none');
         }
@@ -263,13 +267,13 @@ function adicionarVagaFavorita() {
     console.log(todosBtnFavorito);
     const usuarioSession = sessionStorage.getItem('usuario-login');
     const id_usuario = JSON.parse(usuarioSession).id;
-    for(let btnFavorito of todosBtnFavorito) {
+    for (let btnFavorito of todosBtnFavorito) {
         btnFavorito.addEventListener("click", (e) => {
             const el = e.target;
             let objDados = leDados();
-            for(let obj of objDados) {
-                if(el.classList.contains(String(obj.id))) {
-                    favoritosArray.push({...obj, id_usuario: id_usuario});
+            for (let obj of objDados) {
+                if (el.classList.contains(String(obj.id))) {
+                    favoritosArray.push({ ...obj, id_usuario: id_usuario });
                     localStorage.setItem('favoritos', JSON.stringify(favoritosArray));
                 }
             }
--- Codigo/templategabriel/js/index_vaga_info.js
+++ Codigo/templategabriel/js/index_vaga_info.js
@@ -7,16 +7,52 @@ function exibirVaga() {
         if(vagaInfo.id === vagaId) {
             str += 
             `
-                <div class="p-3 rounded">
-                    <h3 class="text-primary">${vagaInfo.funcao}</h3>
-                    <h5 class="text-secondary">${vagaInfo.atividades}</h5>
-                    <div class="">
-                    
-                    </div>
+                <div class="d-flex justify-content-between align-items-end">
+                    <h2 class="text-primary">${vagaInfo.funcao}</h2>
+                    <h5 class="bg-primary text-white p-2 rounded">${vagaInfo.status}</h5>
+                </div>
+                
+                <h5 class="mb-4">${vagaInfo.empresa}</h5>
+
+                <div class="mb-2">
+                    <label class="bold"><strong>Atividades: </strong></label>
+                    <p class="text-secondary text-justify">${vagaInfo.atividades}</p>
+                </div>
+
+                <div class="mb-2">
+                    <label class="bold"><strong>Requisitos: </strong></label>
+                    <p class="text-secondary text-justify">${vagaInfo.requisitos}</p>
+                </div>
+
+                <div class="d-flex flex-column mb-2">
+                    <span><strong>Salario: </strong>${vagaInfo.salario}</span>
+                    <span><strong>Filtro: </strong>${vagaInfo.filtro}</span>
+                    <span><strong>Escolaridade: </strong>${vagaInfo.escolaridade}</span>
+                    <span><strong>Localidade: </strong>${vagaInfo.localidade}</span>
+                    <span><strong>Data: </strong>${vagaInfo.data}</span>
                 </div>
             `;
-        }
+
+            if(vagaInfo.status === "Aberta") {
+                str +=
+                `
+                    <div class="d-flex justify-content-end">
+                        <button class="btn btn-primary">Candidatar-se</button>
+                    </div>
+                `
+            }
+
+            if(vagaInfo.status !== "Aberta") {
+                str +=
+                `
+                    <div class="d-flex justify-content-end">
+                        <button class="btn btn-secondary" disabled>Fechado</button>
+                    </div>
+                `
+            }
+        } 
     }
+
     conteudoPagina.innerHTML = str;
 }
 exibirVaga();
