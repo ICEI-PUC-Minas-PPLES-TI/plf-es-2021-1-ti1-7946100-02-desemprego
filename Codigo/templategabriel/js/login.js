6fc959013c12b0292fe61ed548aa4a60ceb83b50
GabrielMart-hub <gabriel.pereira.1326853@sga.pucminas.br>
2021-07-07 16:21:51 -0300

    .

--- Codigo/templategabriel/js/login.js
+++ Codigo/templategabriel/js/login.js
@@ -28,6 +28,7 @@ onload = () => {
 
 function logar(usuario, senha, alertUsuarioInexistente, userLocalStorage) {
     for(let usuarioCadastrado of userLocalStorage) {
+        console.log(usuarioCadastrado);
         if((usuario.value !== usuarioCadastrado.nome || usuario.value !== usuarioCadastrado.email) && senha.value !== usuarioCadastrado.senha) {
             console.log(usuario.value, usuarioCadastrado.nome, usuarioCadastrado.email);
             console.log(senha.value, usuarioCadastrado.senha);
